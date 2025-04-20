e86f52e3fe6815e973ea122eab2615eeb5498240
chetanghate <codesnippet.java@gmail.com>
2025-04-20 17:22:28 +0530

    Spring Security JWT Auth Changes

--- src/main/java/com/codesnippet/weather_service/config/SecurityConfig.java
+++ src/main/java/com/codesnippet/weather_service/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.codesnippet.weather_service.config;
 
+import com.codesnippet.weather_service.filters.JwtAuthFilter;
 import com.codesnippet.weather_service.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,19 +15,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->
-                        auth.requestMatchers("/h2-console").permitAll()
-                                .anyRequest().authenticated())
-                .httpBasic(withDefaults());
+                        auth.requestMatchers("/authenticate").permitAll()
+                                .anyRequest().authenticated());
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
     @Bean
