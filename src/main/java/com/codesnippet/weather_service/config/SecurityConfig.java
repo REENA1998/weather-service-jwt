ddd156cb26ca0f8a7a24a1ec69699b01439f0333
chetanghate <codesnippet.java@gmail.com>
2025-06-14 23:24:31 +0530

    Authorization And User Management

--- src/main/java/com/codesnippet/weather_service/config/SecurityConfig.java
+++ src/main/java/com/codesnippet/weather_service/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +25,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Autowired
     JwtAuthFilter jwtAuthFilter;
@@ -32,9 +34,11 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->
                         auth.requestMatchers("/authenticate").permitAll()
-                                .requestMatchers(HttpMethod.GET,"/weather/**").hasAuthority(Permissions.WEATHER_READ.name())
-                                .requestMatchers(HttpMethod.POST,"/weather/**").hasAuthority(Permissions.WEATHER_WRITE.name())
-                                .requestMatchers(HttpMethod.DELETE,"/weather/**").hasAuthority(Permissions.WEATHER_DELETE.name())
+                                .requestMatchers("/api/users/register").permitAll()
+
+                                //.requestMatchers(HttpMethod.GET,"/weather/**").hasAuthority(Permissions.WEATHER_READ.name())
+                                //.requestMatchers(HttpMethod.POST,"/weather/**").hasAuthority(Permissions.WEATHER_WRITE.name())
+                                //.requestMatchers(HttpMethod.DELETE,"/weather/**").hasAuthority(Permissions.WEATHER_DELETE.name())
                                 .anyRequest().authenticated());
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
--- src/main/java/com/codesnippet/weather_service/controller/WeatherController.java
+++ src/main/java/com/codesnippet/weather_service/controller/WeatherController.java
@@ -6,6 +6,7 @@ import com.codesnippet.weather_service.repository.WeatherRepository;
 import com.codesnippet.weather_service.service.CacheInspectionService;
 import com.codesnippet.weather_service.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class WeatherController {
     CacheInspectionService cacheInspectionService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public String getWeather(@RequestParam String city) {
         String weatherByCity
                 = weatherService.getWeatherByCity(city);
@@ -30,6 +32,7 @@ public class WeatherController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('WEATHER_WRITE')")
     public Weather addWeather(@RequestBody Weather weather) {
         return weatherRepository.save(weather);
     }
@@ -47,6 +50,7 @@ public class WeatherController {
         return weatherService.updateWeather(city, weatherUpdate);
     }
     @DeleteMapping("/{city}")
+    @PreAuthorize("hasAuthority('WEATHER_DELETE')")
     public String deleteWeather(@PathVariable String city) {
         weatherService.deleteWeather(city);
         return "Weather data for " + city + " has been deleted and cache evicted.";
